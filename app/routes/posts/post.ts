f2f32c32bc0c1a1b6993979d83961b99787810d6
Dane Miller <khadane.miller@gmail.com>
2021-12-02 23:06:43 -0500

    remix tutorial part 1

---
+++ app/routes/posts/post.ts
@@ -0,0 +1,34 @@
+import path from "path";
+import fs from "fs/promises";
+import parseFrontMatter from "front-matter";
+import invariant from "tiny-invariant";
+import { marked } from "marked";
+
+export type Post = {
+  slug: string;
+  title: string;
+};
+
+export type PostMarkdownAttributes = {
+  title: string;
+};
+
+function isValidPostAttributes(
+  attributes: any
+): attributes is PostMarkdownAttributes {
+  return attributes?.title;
+}
+
+const postsPath = path.join(__dirname, "..", "posts");
+
+export async function getPost(slug: string) {
+  const filepath = path.join(postsPath, slug + ".md");
+  const file = await fs.readFile(filepath);
+  const { attributes, body } = parseFrontMatter(file.toString());
+  invariant(
+    isValidPostAttributes(attributes),
+    `Post ${filepath} is missing attributes`
+  );
+  const html = marked(body);
+  return { slug, html, title: attributes.title };
+}
